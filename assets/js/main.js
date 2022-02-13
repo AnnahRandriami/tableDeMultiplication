cbe1926d13fdb3ff579d992b00b48668458f02cd
AnnahRandriami <annah.randriami@gmail.com>
2022-02-13 18:31:34 +0100

    tableMulti

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+/*var multi = 0;
+var num = prompt();
+while (multi<11) {
+    var reponse = (num * multi);
+    console.log(num+"x"+multi+"="+reponse );
+    multi++
+} */
+
+/*var num = prompt();
+for (let multi = 0; multi <= 10; multi++){
+    var reponse = (num * multi);
+    console.log(num+"x"+multi+"="+reponse );
+}*/
+
+/*var multi=0;
+var num = prompt();
+do {
+    var reponse = (num * multi);
+    console.log(num+"x"+multi+"="+reponse );
+    multi++
+} while (multi <= 10);*/
+
+/*var table_multiplication =(nombre, limit = 12) => {
+    if (typeof (limit)!=="number"){
+            limit=12;
+    }
+if (nombre && typeof(nombre)=== "number") {
+    for (let index = 0; index <= limit; index++) {
+        console.log(`${nombre} x ${index} = ${index*nombre}`);
+     }
+}else{
+    console.log("le paramètre doit etre un nombre");
+      }
+   
+}*/
+ 
